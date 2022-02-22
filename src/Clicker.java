d19b5683ffb7ec02ded05fd9a3d710c9f0443a61
Artem Ivanchikov <artem.ivanchikov.99@mail.ru>
2022-02-22 15:05:13 +0300

    create main Clicker class

--- src/Clicker.java
+++ src/Clicker.java
@@ -1,48 +1,51 @@
 import java.awt.*;
 import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 
 public class Clicker {
     public static void main(String[] args) {
-        Robot bot = null;
+        Robot r = null;
         try {
-            bot = new Robot();
+            r = new Robot();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        //default cursor pos
-        bot.mouseMove(653,929);
-        bot.delay(3000);
-        int mask = InputEvent.BUTTON1_DOWN_MASK;
-        for (int i = 0; i < 4; i++) {
-            bot.mouseMove(653,929);
-            bot.mousePress(mask);
-            bot.delay(500);
-            bot.mouseRelease(mask);
-            System.out.println("Delete button pressed");
-            bot.delay(1000);
-            bot.mouseMove(878, 582);
-            //cursor pos
-            System.out.println(MouseInfo.getPointerInfo().getLocation());
-            bot.mousePress(mask);
-            bot.delay(500);
-            bot.mouseRelease(mask);
-            System.out.println("Confirm button pressed");
-            bot.delay(2000);
-        }
-
-        //select name field
-        bot.mouseMove(513,264);
-        bot.mousePress(mask);
-        bot.delay(500);
-        bot.mouseRelease(mask);
-        bot.delay(1000);
+        //default cursor position
+        assert r != null;
+        r.mouseMove(653,929);
+        r.delay(3000);
+        int maskLeftClick = InputEvent.BUTTON1_DOWN_MASK;
         int maskRightClick = InputEvent.BUTTON3_DOWN_MASK;
-        bot.mousePress(maskRightClick);
-        bot.mouseRelease(maskRightClick);
-        bot.delay(500);
-        bot.mouseMove(599,395);
-        bot.mousePress(mask);
-        bot.delay(500);
-        bot.mouseRelease(mask);
+        for (int i = 0; i < 4; i++) {
+            r.mouseMove(653,929);
+            r.mousePress(maskLeftClick);
+            r.delay(500);
+            r.mouseRelease(maskLeftClick);
+            System.out.println("Delete button pressed");
+            r.delay(1000);
+            r.mouseMove(878, 582);
+            //cursor position
+            //System.out.println(MouseInfo.getPointerInfo().getLocation());
+            r.mousePress(maskLeftClick);
+            r.delay(500);
+            r.mouseRelease(maskLeftClick);
+            System.out.println("Confirm button pressed");
+            r.delay(2000);
+        }
+        //select name field
+        r.mouseMove(513,264);
+        r.mousePress(maskLeftClick);
+        r.delay(500);
+        r.mouseRelease(maskLeftClick);
+        r.delay(1000);
+        r.mousePress(maskRightClick);
+        r.mouseRelease(maskRightClick);
+        r.delay(500);
+        r.mouseMove(599,395);
+        r.mousePress(maskLeftClick);
+        r.delay(500);
+        r.mouseRelease(maskLeftClick);
+        r.keyPress(KeyEvent.VK_BACK_SPACE);
+        r.delay(500);
     }
 }
